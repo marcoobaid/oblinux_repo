8c223016a010b2c5c154ac5f1fc9662a774ed311
Marco Obaid <marco.obaid@gmail.com>
2020-01-10 09:43:31 -0600

    Initial commit

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -9,7 +9,7 @@ echo
 echo "Run repo-add"
 echo 
 
-repo-add oblinux-repo.db.tar.gz *.pkg.tar.xz
+repo-add oblinux_repo.db.tar.gz *.pkg.tar.xz
 
 echo "####################################"
 echo "Repo Updated!!"
