f0606a4aada9d6b77931b41432a23aa5f27af15e
Marco ObaID <marco.obaid@gmail.com>
2020-08-08 12:01:16 -0500

    oblinux-calamares-config v20.8.8. Improved update_repo.sh script

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -8,8 +8,9 @@ rm oblinux_repo*
 echo
 echo "Run repo-add"
 echo 
-
-repo-add oblinux_repo.db.tar.gz *.pkg.tar.zst
+#repo-add -s -n -R oblinux_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R oblinux_repo.db.tar.gz *.pkg.tar.zst
+sleep 5
 
 echo "####################################"
 echo "Repo Updated!!"
